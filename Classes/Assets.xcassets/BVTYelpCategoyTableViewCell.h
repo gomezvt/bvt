1e6d837bd09587d7af8446950038f56362307c44
gomezvt <greg@gomez.ws>
2017-02-06 19:51:57 -0500

    Add set up for yelp category cell

---
+++ Classes/Assets.xcassets/BVTYelpCategoyTableViewCell.h
@@ -0,0 +1,13 @@
+***REMOVED***
+***REMOVED***  BVTYelpCategoyTableViewCell.h
+***REMOVED***  bvt
+***REMOVED***
+***REMOVED***  Created by Greg on 2/6/17.
+***REMOVED***  Copyright © 2017 gms. All rights reserved.
+***REMOVED***
+
+#import <UIKit/UIKit.h>
+
+@interface BVTYelpCategoyTableViewCell : UITableViewCell
+
+***REMOVED***
