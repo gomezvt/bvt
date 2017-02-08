e0d47d82912ff0d653bcba53845f4f151dbb64c9
gomezvt <greg@gomez.ws>
2017-02-08 00:49:13 -0500

    add new graphics

--- Classes/BVTThumbNailTableViewCell.m
+++ Classes/BVTThumbNailTableViewCell.m
@@ -10,6 +10,23 @@
 
 #import "YLPLocation.h"
 
+NSString *const star_zero_mini          = @"star_zero_mini.png";
+NSString *const star_one_mini           = @"star_one_mini.png";
+NSString *const star_one_half_mini      = @"star_one_half_mini.png";
+NSString *const star_two_mini           = @"star_two_mini.png";
+NSString *const star_two_half_mini      = @"star_two_half_mini.png";
+NSString *const star_three_mini         = @"star_three_mini.png";
+NSString *const star_three_half_mini    = @"star_three_half_mini.png";
+NSString *const star_four_mini          = @"star_four_mini.png";
+NSString *const star_four_half_mini     = @"star_four_half_mini.png";
+NSString *const star_five_mini          = @"star_five_mini.png";
+
+@interface BVTThumbNailTableViewCell ()
+
+@property (nonatomic, weak) IBOutlet UIImageView *ratingStarsView;
+
+***REMOVED***
+
 @implementation BVTThumbNailTableViewCell
 
 - (void)awakeFromNib
@@ -35,6 +52,51 @@
         self.addressLabel.text = [NSString stringWithFormat:@"%@, %@ %@", location.city, location.stateCode, location.postalCode];
         [self.addressLabel2 removeFromSuperview];
     ***REMOVED***
+    
+    NSString *ratingString;
+    if (self.business.rating == 0)
+    ***REMOVED***
+        ratingString = star_zero_mini;
+    ***REMOVED***
+    else if (self.business.rating == 1)
+    ***REMOVED***
+        ratingString = star_one_mini;
+    ***REMOVED***
+    else if (self.business.rating == 1.5)
+    ***REMOVED***
+        ratingString = star_one_half_mini;
+    ***REMOVED***
+    else if (self.business.rating == 2)
+    ***REMOVED***
+        ratingString = star_two_mini;
+    ***REMOVED***
+    else if (self.business.rating == 2.5)
+    ***REMOVED***
+        ratingString = star_two_half_mini;
+    ***REMOVED***
+    else if (self.business.rating == 3)
+    ***REMOVED***
+        ratingString = star_three_mini;
+    ***REMOVED***
+    else if (self.business.rating == 3.5)
+    ***REMOVED***
+        ratingString = star_three_half_mini;
+    ***REMOVED***
+    else if (self.business.rating == 4)
+    ***REMOVED***
+        ratingString = star_four_mini;
+    ***REMOVED***
+    else if (self.business.rating == 4.5)
+    ***REMOVED***
+        ratingString = star_four_half_mini;
+    ***REMOVED***
+    else
+    ***REMOVED***
+        ***REMOVED*** 5 star rating
+        ratingString = star_five_mini;
+    ***REMOVED***
+    
+    [self.ratingStarsView setImage:[UIImage imageNamed:ratingString]];
 ***REMOVED***
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
