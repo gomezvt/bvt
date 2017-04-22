aad6e669eeab05efe9e262e5c9d873a774c5db4c
gomezvt <greg@gomez.ws>
2017-04-21 23:22:37 -0400

    fix shopping cart row deletion

---
+++ Classes/BVTSurpriseRecommendationsTableViewCell.h
@@ -0,0 +1,13 @@
+***REMOVED***
+***REMOVED***  BVTSurpriseRecommendationsTableViewCell.h
+***REMOVED***  bvt
+***REMOVED***
+***REMOVED***  Created by Greg on 4/21/17.
+***REMOVED***  Copyright © 2017 gms. All rights reserved.
+***REMOVED***
+
+#import <UIKit/UIKit.h>
+
+@interface BVTSurpriseRecommendationsTableViewCell : UITableViewCell
+
+***REMOVED***
