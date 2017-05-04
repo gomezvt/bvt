47338c174947ea0360c32a023de45227dfa069ad
gomezvt <greg@gomez.ws>
2017-05-03 23:59:13 -0400

    fix business selection

--- Classes/BVTSubCategoryTableViewController.m
+++ Classes/BVTSubCategoryTableViewController.m
@@ -483,14 +483,14 @@ static NSString *const kShowDetailSegue = @"ShowDetail";
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    YLPBusiness *business = [self.filteredResults objectAtIndex:indexPath.row];
-    NSArray *values = self.cachedDetails[self.subCategoryTitle];
-
-    
-    YLPBusiness *cachedBiz = [[values filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"phone = %@", business.phone]] lastObject];
-    if (cachedBiz)
+    YLPBusiness *business;   
+    if (self.gotDetails)
     ***REMOVED***
-        business = cachedBiz;
+        business = [self.displayArray objectAtIndex:indexPath.row];
+    ***REMOVED***
+    else
+    ***REMOVED***
+        business = [self.filteredResults objectAtIndex:indexPath.row];
     ***REMOVED***
     
     [[AppDelegate sharedClient] reviewsForBusinessWithId:business.identifier
@@ -563,17 +563,10 @@ static NSString *const kShowDetailSegue = @"ShowDetail";
     cell.tag = indexPath.row;
     
     YLPBusiness *business;
-    
 
     if (self.gotDetails)
     ***REMOVED***
         business = [self.displayArray objectAtIndex:indexPath.row];
-
-***REMOVED***        YLPBusiness *cachedBiz = [[values filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"phone = %@", business.phone]] lastObject];
-***REMOVED***        if (cachedBiz)
-***REMOVED***        ***REMOVED***
-***REMOVED***            business = cachedBiz;
-***REMOVED***        ***REMOVED***
     ***REMOVED***
     else
     ***REMOVED***
