ddba7370c195bdeee38e66fc308ef1368ccae70b
gomezvt <greg@gomez.ws>
2017-05-16 20:52:48 -0400

    add tab bar graphics

---
+++ Classes/BVTTabBarController.h
@@ -0,0 +1,13 @@
+***REMOVED***
+***REMOVED***  BVTTabBarController.h
+***REMOVED***  bvt
+***REMOVED***
+***REMOVED***  Created by Greg on 5/14/17.
+***REMOVED***  Copyright © 2017 gms. All rights reserved.
+***REMOVED***
+
+#import <UIKit/UIKit.h>
+
+@interface BVTTabBarController : UITabBarController
+
+***REMOVED***
---
+++ Classes/BVTTabBarController.m
@@ -0,0 +1,40 @@
+***REMOVED***
+***REMOVED***  BVTTabBarController.m
+***REMOVED***  bvt
+***REMOVED***
+***REMOVED***  Created by Greg on 5/14/17.
+***REMOVED***  Copyright © 2017 gms. All rights reserved.
+***REMOVED***
+
+#import "BVTTabBarController.h"
+
+#import "BVTStyles.h"
+
+@interface BVTTabBarController ()
+
+***REMOVED***
+
+@implementation BVTTabBarController
+
+- (void)viewDidLoad ***REMOVED***
+    [super viewDidLoad];
+    
+    ***REMOVED*** Do any additional setup after loading the view.
+***REMOVED***
+
+- (void)didReceiveMemoryWarning ***REMOVED***
+    [super didReceiveMemoryWarning];
+    ***REMOVED*** Dispose of any resources that can be recreated.
+***REMOVED***
+
+/*
+#pragma mark - Navigation
+
+***REMOVED*** In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender ***REMOVED***
+    ***REMOVED*** Get the new view controller using [segue destinationViewController].
+    ***REMOVED*** Pass the selected object to the new view controller.
+***REMOVED***
+*/
+
+***REMOVED***
