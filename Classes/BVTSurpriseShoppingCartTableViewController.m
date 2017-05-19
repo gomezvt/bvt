e623cb33debd76bf1fad83bf59f009a01e07210c
gomezvt <greg@gomez.ws>
2017-05-18 21:15:10 -0400

    organize storyboard

--- Classes/BVTSurpriseShoppingCartTableViewController.m
+++ Classes/BVTSurpriseShoppingCartTableViewController.m
@@ -331,6 +331,7 @@ static NSString *const kTableViewSectionHeaderView = @"BVTTableViewSectionHeader
             ***REMOVED***
                 dispatch_async(dispatch_get_main_queue(), ^***REMOVED***
                     ***REMOVED*** code here
+                    [self _hideHUD];
                     [self.goButton setEnabled:YES];
                     [self.clearButton setEnabled:YES];
                     
