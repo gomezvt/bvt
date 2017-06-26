6df7595773a429f1f9aea8c4ee5577e7ab02bd06
gomezvt <greg@gomez.ws>
2017-06-25 20:55:19 -0400

    Set up ad banner

--- Classes/BVTDisclaimerViewController.m
+++ Classes/BVTDisclaimerViewController.m
@@ -9,8 +9,11 @@
 #import "BVTDisclaimerViewController.h"
 #import "BVTHeaderTitleView.h"
 #import "BVTStyles.h"
+***REMOVED***
 
 @interface BVTDisclaimerViewController ()
+@property (nonatomic, strong) GADBannerView *bannerView;
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
 
 ***REMOVED***
 
@@ -32,6 +35,28 @@ static NSString *const kHeaderTitleViewNib = @"BVTHeaderTitleView";
 
 - (void)viewDidLoad ***REMOVED***
     [super viewDidLoad];
+    
+    self.tableView.tableFooterView = [UIView new];
+    self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 60.f, 0);
+
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 44.f;
+    
+    UIView *view = self.tabBarController.selectedViewController.view;
+    UIView *bannerSpace = [[UIView alloc] initWithFrame:CGRectMake(0, view.frame.size.height - 61.f, view.frame.size.width, 61.f)];
+    bannerSpace.backgroundColor = [UIColor whiteColor];
+    [view addSubview:bannerSpace];
+    
+    self.bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeFullBanner];
+    
+    [bannerSpace addSubview:self.bannerView];
+    
+    [self.bannerView setFrame:CGRectMake(0, 0, bannerSpace.frame.size.width, self.bannerView.frame.size.height)];
+    
+    self.bannerView.adUnitID = @"ca-app-pub-3940256099942544/2934735716";
+    self.bannerView.rootViewController = self;
+    [self.bannerView loadRequest:[GADRequest request]];
+    
     ***REMOVED*** Do any additional setup after loading the view.
 ***REMOVED***
 
@@ -43,6 +68,24 @@ static NSString *const kHeaderTitleViewNib = @"BVTHeaderTitleView";
 - (void)didReceiveMemoryWarning ***REMOVED***
     [super didReceiveMemoryWarning];
     ***REMOVED*** Dispose of any resources that can be recreated.
+***REMOVED***
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+***REMOVED***
+    return 1;
+***REMOVED***
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+***REMOVED***
+    return 1;
+***REMOVED***
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
+***REMOVED***
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    return cell;
+    
 ***REMOVED***
 
 /*
